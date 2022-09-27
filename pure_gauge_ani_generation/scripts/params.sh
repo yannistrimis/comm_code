dc06fff946da36f5f72f43eb14a918bf3d84a9ec
yannistrimis <trimisio@msu.edu>
2022-09-27 16:41:19 -0400

    build

--- pure_gauge_ani_generation/scripts/params.sh
+++ pure_gauge_ani_generation/scripts/params.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-init_seed=7692
-n_of_lat=2
+init_seed=7562
+n_of_lat=4
 
-nx=4
-ny=4
-nz=4
-nt=8
+nx=24
+ny=24
+nz=24
+nt=48
 
 # We want to reproduce arxiv 1205.0781 for: beta=6.1, xi_0=2.46, 24^3x48
 # MILC convention (in the improved action) is: beta=10/g^2
@@ -18,8 +18,8 @@ nt=8
 # beta_s=beta/xi_0=4.133
 # beta_t=beta*xi_0=25.010
 
-beta=10.167
-xi_0=2.46
+# beta=10.167
+# xi_0=2.46
 
 beta_s=4.133 #in the MILC colde this appears first
 beta_t=25.010 #and this appears second
@@ -28,11 +28,11 @@ beta_name="10167"
 xi_0_name="246"
 
 warms=0
-trajecs=1
+trajecs=4
 traj_between_meas=1
 u0=1.0
-steps_per_trajectory=1
-qhb_steps=1
+steps_per_trajectory=3
+qhb_steps=2
 
 stream="a"
 
@@ -42,6 +42,6 @@ out_name="out${ensemble}"
 lat_name="l${ensemble}"
 
 #directory="/mnt/scratch/trimisio/lattices/lat${ensemble}" #this is for iCER
-#directory="/home/trimis/Code/lat${ensemble}" #this is for workstation
+#directory="/home/trimis/local_code/lat${ensemble}" #this is for workstation
 directory="/home/yannis/Physics/LQCD/code_local/lat${ensemble}" #this is for laptop
 erase="yes"
