6c666f58bb9a69d372691bf10aae0f43a082407f
yannistrimis <trimisio@msu.edu>
2022-10-06 18:10:23 -0400

    minor

--- wilson_flow_ani/scripts_c/params.sh
+++ wilson_flow_ani/scripts_c/params.sh
@@ -2,14 +2,14 @@
 
 n_of_lat=4
 
-nx=24
-ny=24
-nz=24
-nt=48
+nx=4
+ny=4
+nz=4
+nt=8
 
 beta_name="10167"
 xi_0_name="246"
-stream="c"
+stream="a"
 ensemble="${nx}${nt}b${beta_name}x${xi_0_name}${stream}"
 lat_name="l${ensemble}"
 
@@ -23,12 +23,12 @@ flow_action="wilson"
 
 f_ensemble="wflow${nx}${nt}b${beta_name}x${xi_0_name}xf${xf_name}${stream}_dt${dt}"
 
-directory="/mnt/scratch/trimisio/flows/${f_ensemble}" #this is for hpcc
-#directory="/home/trimis/local_code/${f_ensemble}" #this is for workstation
+#directory="/mnt/scratch/trimisio/flows/${f_ensemble}" #this is for hpcc
+directory="/home/trimis/local_code/${f_ensemble}" #this is for workstation
 #directory="/home/yannis/Physics/LQCD/local_code/${f_ensemble}" #this is for laptop
 
-lat_directory="/mnt/scratch/trimisio/lattices/lat${ensemble}" #this is for hpcc
-#lat_directory="/home/trimis/local_code/lat${ensemble}" #this is for workstation
+#lat_directory="/mnt/scratch/trimisio/lattices/lat${ensemble}" #this is for hpcc
+lat_directory="/home/trimis/local_code/lat${ensemble}" #this is for workstation
 #lat_directory="/home/yannis/Physics/LQCD/local_code/lat${ensemble}" #this is for laptop
 
 erase="yes"
