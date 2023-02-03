2b7b9eb098b66620b7cee5ad3541dc622413768a
yannistrimis <trimisio@msu.edu>
2023-02-03 18:27:16 -0500

    hpcc spec analysis start

---
+++ pure_gauge_spectrum/analysis/cleaning_script.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+cur_dir = '/mnt/home/trimisio/outputs'
+vol = '2080'
+beta = '7433'
+x0 = '35221'
+stream = 'a'
+i_file = '101'
+f_read = open(,'r')
+
+flag = 0
+
+f_read.close()
