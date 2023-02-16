88dcc8251271a23e0b53854ea81bd09d45b526eb
yannistrimis <trimisio@msu.edu>
2023-02-16 11:37:39 -0500

    clean_one ready

--- pure_gauge_spectrum/analysis/clean_one.py
+++ pure_gauge_spectrum/analysis/clean_one.py
@@ -5,18 +5,33 @@ vol = '1616'
 beta = '7000'
 x0 = '100'
 stream = 'a'
-i_file = input()
-mass_line = input()
-source_line = input()
-sink_line = input()
+
+#i_file = input()
+#mass1 = input()
+#mass2 = input()
+#source1 = input()
+#source2 = input()
+#sinks = input()
+
+i_file = '101'
+mass1 = '0.02'
+mass2 = '0.02'
+source1 = 'CORNER'
+source2 = 'CORNER'
+sinks = 'PION_5'
+
 ens_name = vol+'b'+beta+'x'+x0+stream
 f_read = open('%s/l%s/spec%s.lat.%s'%(cur_dir,ens_name,ens_name,i_file),'r')
-f_write_a = open('%s/l%s/clean_spec%s.lat.%sa'%(cur_dir,ens_name,ens_name,i_file),'w')
-f_write_b = open('%s/l%s/clean_spec%s.lat.%sb'%(cur_dir,ens_name,ens_name,i_file),'w')
+f_write_a = open('%s/l%s/spec_m1_%s_m2_%s_%s_%s_%s.%sa'%(cur_dir,ens_name,mass1,mass2,sinks,source1,source2,i_file),'w')
+f_write_b = open('%s/l%s/spec_m1_%s_m2_%s_%s_%s_%s.%sb'%(cur_dir,ens_name,mass1,mass2,sinks,source1,source2,i_file),'w')
 
 source_flag = 0
 flag = 0
 counter = 0
+
+mass_line = 'MASSES:' + ' ' + mass1 + ' ' + mass2 + ' '
+source_line = 'SOURCE:' + ' ' + source1 + ' ' +  source2
+sink_line = 'SINKS:' + ' ' + sinks
 
 content = f_read.readlines()
 for line in content:
@@ -25,27 +40,30 @@ for line in content:
 	if split_line[0]=='#' and split_line[1]=='source' and split_line[2]=='time':
 		source_flag = source_flag + 1
 
-	if line == 'STARTPROP\n' :
+	if line.rstrip('\n') == 'STARTPROP' :
 		flag = 1
-	elif flag == 1 and line == mass_line :
+	elif flag==1 and line.rstrip('\n')==mass_line :
 		flag = 2
-	elif flag == 2 and line == source_line :
-		flag = 3
-	elif flag ==3 and line == sink_line :
-	elif flag == 3 and split_line[0] == '0' :
+	elif flag==2 and line.rstrip('\n')==source_line :
+		flag = 3		
+	elif flag==3 and line.rstrip('\n')==sink_line :
+		print(source_flag)
+		print('i am in')
 		flag = 4
+	elif flag==4 and split_line[0]=='0' :
+		flag = 5
 		counter = 1
 		if source_flag == 1 :
 			f_write_a.write('%s'%(line))
 		elif source_flag == 2 :
 			f_write_b.write('%s'%(line))
-	elif flag == 4 and split_line[0] == str(counter) :
+	elif flag==5 and split_line[0]==str(counter) :
 		counter = counter + 1
 		if source_flag == 1 :
 			f_write_a.write('%s'%(line))
 		elif source_flag == 2 :
 			f_write_b.write('%s'%(line))
-	elif flag == 4 and line == 'ENDPROP\n' :
+	elif flag==5 and line.rstrip('\n')=='ENDPROP' :
 		flag = 0
 		counter = 0
 
