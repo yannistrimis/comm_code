785ceb61512cb36b5e014396b6f227bf14235faa
yannistrimis <trimisio@msu.edu>
2023-03-03 18:58:56 -0500

    hpcc timely

--- pure_gauge_spectrum/analysis/format_corrfitter_one.py
+++ pure_gauge_spectrum/analysis/format_corrfitter_one.py
@@ -17,10 +17,10 @@ sinks = input()
 
 ens_name = vol+'b'+beta+'x'+x0+stream
 
-f_write = open('%s/l%s/m1_%s_m2_%s_%s_%s_%s.data'%(out_dir,ens_name,mass1,mass2,sinks,source1,source2),'w')
+f_write = open('%s/l%s/m1_%s_m2_%s_%s_%s_%s.fold.data'%(out_dir,ens_name,mass1,mass2,sinks,source1,source2),'w')
 
-for i_file in range(101,201):
-    f_read = open('%s/l%s/spec_m1_%s_m2_%s_%s_%s_%s.%d'%(out_dir,ens_name,mass1,mass2,sinks,source1,source2,i_file),'r')
+for i_file in range(101,300):
+    f_read = open('%s/l%s/foldspec_m1_%s_m2_%s_%s_%s_%s.%d'%(out_dir,ens_name,mass1,mass2,sinks,source1,source2,i_file),'r')
     content = f_read.readlines()
 
     f_write.write( 'PROP' )
--- pure_gauge_spectrum/analysis/my_fitter.py
+++ pure_gauge_spectrum/analysis/my_fitter.py
@@ -20,50 +20,50 @@ sinks = input()
 
 cur_dir = '/mnt/home/trimisio/plot_data/spec_data'
 
-f_read = open('%s/l%s/m1_%s_m2_%s_%s_%s_%s.data'%(cur_dir,ens_name,mass1,mass2,sinks,source1,source2),'r')
+f_read = open('%s/l%s/m1_%s_m2_%s_%s_%s_%s.fold.data'%(cur_dir,ens_name,mass1,mass2,sinks,source1,source2),'r')
 content = f_read.readlines()
 f_read.close()
-tmin = 1
-tmax = 15
+tmin = 2
+# tmax = 15
 n_of_meas = len(content)
-x = np.zeros(tmax-tmin+1)
-for i in range(tmax-tmin+1) :
+x = np.zeros(int(nt/2)-tmin+1)
+for i in range(int(nt/2)-tmin+1) :
     x[i] = float(i) + tmin
 
 def main() :
-    y_arr = np.zeros(( tmax-tmin+1,n_of_meas ))
+    y_arr = np.zeros(( int(nt/2)-tmin+1, n_of_meas ))
 
     for kk in range(n_of_meas) :
         line = content[kk].split(' ')
-        for i in range(tmax-tmin+1) :
+        for i in range(int(nt/2)-tmin+1) :
             y_arr[i,kk] = float(line[i+1+tmin])
  
     y_cov = np.cov(y_arr)
     y_av = np.average(y_arr,axis=1)   
-    p0 = dict(an=-200,En=0.5,ao=150,Eo=0.5)
-#    p0 = dict(an=594,En=0.42)
+#    p0 = dict(an=-200,En=0.5,ao=150,Eo=0.5)
+    p0 = dict(an=500,En=0.5)
 
     fit = lsqfit.nonlinear_fit(data=(x,y_av,y_cov), prior=None, p0=p0, fcn=fitfcn)
-    print('\ntmin = %d   tmax = %d\n'%(tmin,tmax))
+    print('\ntmin = %d\n'%tmin)
     print(fit)
     print('========FIT POINTS AND ERRORS=========')
-    for i in range(tmax-tmin+1) :
+    for i in range(int(nt/2)-tmin+1) :
         print( x[i],fitfcn(x[i],fit.p).mean,fitfcn(x[i],fit.p).sdev )
     print('========MEASUREMENT AVERAGES AND ERRORS=========')
-    for i in range(tmax-tmin+1) :
+    for i in range(int(nt/2)-tmin+1) :
         av = y_av[i]
         err = np.sqrt(y_cov[i,i])
         print(x[i],av,err)
     print("===========DISTANCES============")
-    for i in range(tmax-tmin+1) :
+    for i in range(int(nt/2)-tmin+1) :
         av = y_av[i]
         err = np.sqrt(y_cov[i,i])
         quantity = ( av-fitfcn(x[i],fit.p).mean )**2 / err**2
         print(x[i],quantity)
 
 def fitfcn(x,p) :
-    return p['an']*( np.exp(-p['En']*x)+np.exp(-p['En']*(nt-x)) ) + (-1)**x*p['ao']*( np.exp(-p['Eo']*x)+np.exp(-p['Eo']*(nt-x)) )
-#    return p['an']*( np.exp(-p['En']*x)+np.exp(-p['En']*(nt-x)) )
+#    return p['an']*( np.exp(-p['En']*x)+np.exp(-p['En']*(nt-x)) ) + (-1)**x*p['ao']*( np.exp(-p['Eo']*x)+np.exp(-p['Eo']*(nt-x)) )
+    return p['an']*( np.exp(-p['En']*x)+np.exp(-p['En']*(nt-x)) )
 
 if __name__ == '__main__' :
     main()
