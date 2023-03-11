8f5fb6679bc2b3d896f5ffd85fbcd7aecd1bc532
yannistrimis <trimisio@msu.edu>
2023-03-11 15:16:20 -0500

    laptop timely

--- other/pure_u1/analysis_statpot.py
+++ other/pure_u1/analysis_statpot.py
@@ -9,13 +9,36 @@ beta = 200
 stream = 'a'
 f_read = open('%s/l%d%db%d%s/wl_r_2_4_6_t_2_3_4_5_6.data'%(my_dir,nx,nt,beta,stream),'r')
 content = f_read.readlines()
+f_read.close()
+rr_arr = ['2','4','6']
+tt_arr = ['2','3','4','5','6']
 
 n_of_lat = 60
 nbins = 10
 
-for rr in ['2','4','6'] :
-    for tt in ['2','3','4','5','6'] :
+
+i_col = 0
+for rr in rr_arr : 
+    for tt in tt_arr :        
+
+        f_write_log = open('%s/l%d%db%d%s/wl_r_%s_t_%s.log.re.data'%(my_dir,nx,nt,beta,stream,rr,tt),'w')
+        f_write = open('%s/l%d%db%d%s/wl_r_%s_t_%s.re.data'%(my_dir,nx,nt,beta,stream,rr,tt),'w')
         
+        i_col = i_col + 2
+        for i_line in range(len(content)) :
+            line = content[i_line].split(' ')
+            value = float(line[i_col])
+            value_log = np.log(value)
+            f_write_log.write('%lf\n'%value_log)
+            f_write.write('%lf\n'%value)
+        
+        f_write_log.close()
+        f_write.close()
+
+### THE SECOND PART CREATES ONE FILE PER SPATIAL LENGTH WITH <W(T+1)>/<W(T)>
+### TO ASSESS CONVERGENCE
 
 
-f_read.close()
+
+
+### THE THIRD PART FITS -ln<Re[W(R,T)]> = - V(R)T + C TO EXTRACT POTENTIAL V(R)
