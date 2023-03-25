b642d8bc56a3626c48999ba6a79744cc2c013a29
yannistrimis <trimisio@msu.edu>
2023-03-25 14:05:08 -0400

    laptop scalar

---
+++ other/scalar/c_code/control.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "lattice.h"
+#include "utils.h"
+// #include "action.h"
+
+double* lattice[2];
+int nx;
+int nt;
+int vol;
+double beta;
+double action;
+
+int main(void){
+
+    int n_of_lat;
+    int traj;
+    int my_seed;
+    double d_update;
+
+
+    scanf("seed = %d\n",&my_seed);
+    scanf("nx = %d\n",&nx);
+    scanf("nt = %d\n",&nt);
+    scanf("beta = %lf\n",&beta);
+    scanf("n_of_lat = %d\n",&n_of_lat);
+    scanf("trejectories = %d\n",&traj);
+    scanf("d_update = %lf\n",&d_update);
+
+
+    // printf("seed = %d\n",my_seed);
+    // printf("nx = %d\n",nx);
+    // printf("nt = %d\n",nt);
+    // printf("beta = %lf\n",beta);
+    // printf("n_of_lat = %d\n",n_of_lat);
+    // printf("trejectories = %d\n",traj);
+    // printf("d_update = %lf\n",d_update);
+
+    vol = nx*nx*nx*nt;
+
+    initialize(my_seed);
+    for(int ind=0;ind<vol;ind++){
+        printf("%lf %lf %d\n",lattice[0][ind],lattice[1][ind],ind);
+    }
+    return 0;
+}
---
+++ other/scalar/c_code/lattice.h
@@ -0,0 +1,6 @@
+extern double* lattice[2];
+extern int nx;
+extern int nt;
+extern int vol;
+extern double beta;
+extern double action;
