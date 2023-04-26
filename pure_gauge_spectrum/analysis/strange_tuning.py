f561f4391b85f8468a0b9ff7eca5a9bad996c93c
yannistrimis <trimisio@msu.edu>
2023-04-26 18:20:18 -0400

    hpcc timely

--- pure_gauge_spectrum/analysis/strange_tuning.py
+++ pure_gauge_spectrum/analysis/strange_tuning.py
@@ -1,17 +1,17 @@
 import numpy as np
 from scipy.optimize import fsolve
 
-w0 = 1.082917
-a = 0.17355/w0
+w0 = 1.08297
+a = 0.17355/w0 # FLAG 2021
 
-m_ss = 695
+m_ss = 685.8 # MeV FROM 0910.1229
 m_ss = m_ss/197.3
 m_ss = m_ss*a
 
-a0 = 0.141836
-a1 = 8.17041
-a2 = -51.1507
-a3 = 179.744
+a0 = ???
+a1 = ???
+a2 = ???
+a3 = ???
 
 def func(x):
     return - m_ss + a0 + a1*x + a2*x*x + a3*x*x*x
