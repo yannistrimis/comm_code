eee59900f512d15d48e5c1a2438d6ec4199c2a4b
yannistrimis <trimisio@msu.edu>
2023-06-15 03:26:59 -0400

    hpcc timely

--- pure_gauge_spectrum/analysis/fitter_scripts/meff.py
+++ pure_gauge_spectrum/analysis/fitter_scripts/meff.py
@@ -2,26 +2,23 @@ import numpy as np
 from sympy import cosh, nsolve
 from sympy.abc import x
 
-# THIS HAS TO BE PIPED INTO A FILE CALLED <something>.meff.data
-
-nx = 16
-nt = 32
-vol = str(nx) + str(nt)
+nx=16
+nt=32
+vol = str(nx)+str(nt)
 beta = '6850'
 x0 = '100'
 stream = 'a'
-ens_name = vol+'b'+beta+'x'+x0+stream
 
-mass1 = input()
-mass2 = input()
-sinks = input()
-pre_name = input()
+cur_dir = '/mnt/home/trimisio/plot_data/spec_data/l'+vol+'b'+beta+'x'+x0+stream
 
-cur_dir = '/mnt/home/trimisio/plot_data/spec_data'
 
-f_read = open('%s/l%s/%s_m1_%s_m2_%s_%s.averr'%(cur_dir,ens_name,pre_name,mass1,mass2,sinks),'r')
+f1 = input()
+
+f_read = open('%s/%s.averr'%(cur_dir,f1),'r')
 content = f_read.readlines()
 f_read.close()
+
+f_write = open('%s/%s.coshmeff.data'%(cur_dir,f1),'w')
 
 y_av = np.zeros(int(nt/2)+1)
 
@@ -37,7 +34,10 @@ def main() :
         c1 = t - nt/2
         c2 = c1 + 2 
         f = cosh(x*c1)/cosh(x*c2)-y_av[t]/y_av[t+2]
-        meff[t] = nsolve(f,x,1)
+        if t % 2 == 0 :
+            meff[t] = nsolve(f,x,1)
+            f_write.write( '%d %f\n'%(t,meff[t]) )
+    f_write.close()
 
 if __name__ == '__main__' :
     main()
