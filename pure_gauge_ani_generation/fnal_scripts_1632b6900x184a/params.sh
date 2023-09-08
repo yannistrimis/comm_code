5e39a11851900a46f9d398000deb0482c5822892
Ioannis Trimis <trimisio@lq.fnal.gov>
2023-09-08 11:22:28 -0500

    fnal

--- pure_gauge_ani_generation/fnal_scripts_1632b6900x184a/params.sh
+++ pure_gauge_ani_generation/fnal_scripts_1632b6900x184a/params.sh
@@ -5,7 +5,7 @@ cluster=fnal
 
 
 init_seed=1158
-n_of_lat=200
+n_of_lat=300
 n_of_sub=1
 
 nx=16
@@ -50,7 +50,7 @@ submit_dir="/project/ahisq/yannis_puregauge/submits/subgenl1632b6900x184a"
 
 executable="su3_ora_symzk0_a_dbl_gnu8openmpi3_fnal_20230906"
 
-sbatch_time="03:00:00"
+sbatch_time="05:00:00"
 sbatch_nodes="4"
 sbatch_ntasks="128"
 sbatch_jobname="gen184"
