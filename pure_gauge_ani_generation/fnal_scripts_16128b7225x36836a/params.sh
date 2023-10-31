0b6b7225875de652832d3b6af6db839e31b2f2b5
Ioannis Trimis <trimisio@lq.fnal.gov>
2023-10-31 13:05:19 -0500

    fnal

--- pure_gauge_ani_generation/fnal_scripts_16128b7225x36836a/params.sh
+++ pure_gauge_ani_generation/fnal_scripts_16128b7225x36836a/params.sh
@@ -4,8 +4,8 @@
 cluster=fnal
 
 init_seed=1158
-n_of_lat=5
-n_of_sub=1
+n_of_lat=125
+n_of_sub=4
 
 nx=16
 ny=16
@@ -49,7 +49,7 @@ submit_dir="/project/ahisq/yannis_puregauge/submits/subgenl16128b7225x36836a"
 
 executable="su3_ora_symzk0_a_dbl_gcc12openmpi4_20231030"
 
-sbatch_time="02:00:00"
+sbatch_time="20:00:00"
 sbatch_nodes="4"
 sbatch_ntasks="128"
 sbatch_jobname="gen4"
--- pure_gauge_ani_generation/fnal_scripts_1664b70805x18876a/params.sh
+++ pure_gauge_ani_generation/fnal_scripts_1664b70805x18876a/params.sh
@@ -4,8 +4,8 @@
 cluster=fnal
 
 init_seed=1158
-n_of_lat=5
-n_of_sub=1
+n_of_lat=250
+n_of_sub=2
 
 nx=16
 ny=16
@@ -49,7 +49,7 @@ submit_dir="/project/ahisq/yannis_puregauge/submits/subgenl1664b70805x18876a"
 
 executable="su3_ora_symzk0_a_dbl_gcc12openmpi4_20231030"
 
-sbatch_time="02:00:00"
+sbatch_time="15:00:00"
 sbatch_nodes="4"
 sbatch_ntasks="128"
 sbatch_jobname="gen2"
