5c9c04273b0187690686138f308deff4b127e841
Ioannis Trimis <trimisio@lq.fnal.gov>
2023-11-21 18:02:10 -0600

    fnal

--- pure_gauge_spectrum/fnal_tun_scripts_16128b7225x36836a/params.sh
+++ pure_gauge_spectrum/fnal_tun_scripts_16128b7225x36836a/params.sh
@@ -3,7 +3,7 @@
 
 cluster=fnal
 
-n_of_lat=40
+n_of_lat=20
 n_of_sub=10
 
 set_i_lat=101
@@ -47,7 +47,7 @@ submit_dir="/project/ahisq/yannis_puregauge/submits/subspectunl16128b7225x36836a
 
 executable="ks_spectrum_ani_hisq_dbl_gcc12openmpi4_20231030"
 
-sbatch_time="04:00:00"
+sbatch_time="02:00:00"
 sbatch_nodes="4"
 sbatch_ntasks="128"
 sbatch_jobname="xtun_xi4"
