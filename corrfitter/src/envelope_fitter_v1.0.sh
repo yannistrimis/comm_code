dea77a2805300c426143aa03cfcf72975b58d39d
yannistrimis <trimisio@msu.edu>
2023-11-26 14:42:27 -0500

    laptop

--- corrfitter/src/envelope_fitter_v1.0.sh
+++ corrfitter/src/envelope_fitter_v1.0.sh
@@ -1,50 +1,51 @@
 #!/bin/bash
 
-vol=1664
-beta=70805
-xg=18876
+vol=16128
+beta=7225
+xg=36836
 src="rcw"
-mass="0.02"
+prefix="tun"
+mass="0.05"
 taste="PION_5"
 
 # fitdir="/home/trimis/local_code" # CMSE
 # dir="/home/trimis/fnal/all/spec_data/l${vol}b${beta}x${xg}a" # CMSE
 fitdir="/home/yannis/Physics/LQCD/spec_data" # LAPTOP
 dir="/home/yannis/Physics/LQCD/fnal/all/spec_data/l${vol}b${beta}x${xg}a" # LAPTOP
-tdata=33
-tp=64
+tdata=65
+tp=128
 n_states=1
-m_states=1
+m_states=0
 
 if [ $1 == "scan" ]
 then
 
-xq_arr=( "1880" "1940" "2000" "2060" )
-mom_arr=( "p100" "p110" )
+xq_arr=( "3760" "3880" "4000" )
+mom_arr=( "p000" )
 
 for xq in ${xq_arr[@]};do
 echo "xq = ${xq}"
 for mom in ${mom_arr[@]};do
 echo "	${mom}"
 
-if [ -f ${fitdir}/${mom}${src}${vol}b${beta}x${xg}xq${xq}_m${mass}m${mass}${taste}.${n_states}p${m_states}.scanfit ]
+if [ -f ${fitdir}/${prefix}${mom}${src}${vol}b${beta}x${xg}xq${xq}_m${mass}m${mass}${taste}.${n_states}p${m_states}.scanfit ]
 then
-rm ${fitdir}/${mom}${src}${vol}b${beta}x${xg}xq${xq}_m${mass}m${mass}${taste}.${n_states}p${m_states}.scanfit
+rm ${fitdir}/${prefix}${mom}${src}${vol}b${beta}x${xg}xq${xq}_m${mass}m${mass}${taste}.${n_states}p${m_states}.scanfit
 fi
 
 
-tmin_min=3
-tmin_max=20
+tmin_min=10
+tmin_max=50
 
-tmax_min=33
-tmax_max=33
+tmax_min=65
+tmax_max=65
 
 for ((tmin=${tmin_min};tmin<=${tmin_max};tmin++));do
 for ((tmax=${tmax_min};tmax<=${tmax_max};tmax++));do
 
-python3 fitter_v1.0.py <<EOF >> ${fitdir}/${mom}${src}${vol}b${beta}x${xg}xq${xq}_m${mass}m${mass}${taste}.${n_states}p${m_states}.scanfit
+python3 fitter_v1.0.py <<EOF >> ${fitdir}/${prefix}${mom}${src}${vol}b${beta}x${xg}xq${xq}_m${mass}m${mass}${taste}.${n_states}p${m_states}.scanfit
 ${dir}
-${mom}${src}${vol}b${beta}x${xg}xq${xq}_m${mass}m${mass}${taste}.specdata
+${prefix}${mom}${src}${vol}b${beta}x${xg}xq${xq}_m${mass}m${mass}${taste}.specdata
 ${tmin}
 ${tmax}
 ${tdata}
@@ -63,14 +64,14 @@ done # xq
 elif [ $1 == "one"  ]
 then
 
-xq="1880"
+xq="3880"
 mom="p000"
-tmin=22
-tmax=33
+tmin=0
+tmax=0
 
 python3 fitter_v1.0.py <<EOF
 ${dir}
-${mom}${src}${vol}b${beta}x${xg}xq${xq}_m${mass}m${mass}${taste}.specdata
+${prefix}${mom}${src}${vol}b${beta}x${xg}xq${xq}_m${mass}m${mass}${taste}.specdata
 ${tmin}
 ${tmax}
 ${tdata}
