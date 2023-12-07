4b4b55991cc422dfd201fc7914863421740adc3f
yannistrimis <trimisio@msu.edu>
2023-12-07 13:58:21 -0500

    cmse

--- corrfitter/src/envelope_fitter_v1.0.sh
+++ corrfitter/src/envelope_fitter_v1.0.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-vol=16128
-beta=7225
-xg=36836
-src="rcw"
-prefix="tuncheck"
-taste="PION_5"
-print_state="n0"
+vol=1664
+beta=70805
+xg=18876
+src="eowfw"
+prefix="nlpi"
+taste="PION_05"
+print_state="o0"
 
 fitdir="/home/trimis/spec_data" # CMSE
 dir="/home/trimis/fnal/all/spec_data/l${vol}b${beta}x${xg}a" # CMSE
 # fitdir="/home/yannis/Physics/LQCD/spec_data" # LAPTOP
 # dir="/home/yannis/Physics/LQCD/fnal/all/spec_data/l${vol}b${beta}x${xg}a" # LAPTOP
-tdata=65
-tp=128
-n_states=2
+tdata=33
+tp=64
+n_states=1
 m_states=1
 
 if [ $1 == "scan" ]
 then
 
 tmin_min=0
-tmin_max=45
+tmin_max=27
 
-tmax_min=65
-tmax_max=65
+tmax_min=33
+tmax_max=33
 
-xq_arr=( "3750" )
-mom_arr=( "p100" "p110" )
-mass_arr=( "0.0724" )
+xq_arr=( "1950" )
+mom_arr=( "p000" )
+mass_arr=( "0.01532" )
 
 for xq in ${xq_arr[@]};do
 echo "xq = ${xq}"
@@ -70,10 +70,10 @@ then
 
 xq="1950"
 mom="p000"
-tmin=11
+tmin=25
 tmax=33
 
-mass=0.12
+mass=0.01532
 
 python3 fitter_v1.0.py <<EOF
 ${dir}
